e016494c47a93fbe5d0b2ea0adabfc34a0fc01bc
Juan Pablo Muñoz <juanpablo@lm.net.co>
2018-09-14 15:42:19 -0500

    Inicio con webpack

---
+++ app.js
@@ -0,0 +1,11 @@
+import {uniq} from 'lodash';
+import insane from 'insane';
+import jsonp from 'jsonp';
+import apiKey from './src/config';
+
+console.log(apiKey);
+
+
+const ages = [1,4,52,12,7];
+
+console.log(uniq(ages));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require("webpack");
+const nodeEnv = process.env.NODE_ENV || 'production';
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode : "production",
+  devtool : "source-map",
+  entry : "./app.js",
+  output : {
+    filename : "_build/bundle.js"
+  },
+  module : {
+    rules : [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader : "babel-loader",
+        options : {
+          presets : ["@babel/preset-env"]
+        }
+      }
+    ]
+  },
+  plugins : [
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(true),
+      VERSION: JSON.stringify('5fa3b9'),
+      BROWSER_SUPPORTS_HTML5: true,
+      TWO: '1+1',
+      'typeof window': JSON.stringify('object'),
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
+}
